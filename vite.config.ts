f34a3c084f00b7098d07930b1c94bf174ddaced2
Edoardo Dusi <edo@edoardodusi.com>
2025-02-06 12:43:19 +0100

    chore: align gh actions and dependabot with ecosystem standard, update dev dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vitest/config';
 import { lightGreen } from 'kolorist';
 import banner from 'vite-plugin-banner';
 import dts from 'vite-plugin-dts';
@@ -17,8 +15,8 @@ export default defineConfig({
     }),
     banner({
       content: `/**\n * name: ${pkg.name}\n * (c) ${new Date().getFullYear()}\n * description: ${pkg.description}\n * author: ${pkg.author}\n */`,
-    }),
-  ],
+    }) as unknown as Plugin,
+  ] as Plugin[],
   build: {
     lib: {
       entry: 'src/index.ts',
